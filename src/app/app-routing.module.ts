f54fe095fe0b141dfd999848b9922c9f6ea3f53c
karthik <karthik@karthiks-Air.lan>
2023-04-24 10:36:52 -0400

    env

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MissionListComponent } from './mission-list/mission-list.component';
 import { MissionDetailComponent } from './mission-detail/mission-detail.component'
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'missions', component: MissionListComponent },
   { path: 'missions/:id', component: MissionDetailComponent }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { API_KEY } from './../../../config'
+interface NasaApiResponse {
+  url: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  nasaApiResponse: NasaApiResponse;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<NasaApiResponse>('https://api.nasa.gov/planetary/apod?api_key=API_KEY').subscribe(
+      (response) => {
+        this.nasaApiResponse = response;
+      },
+      (error) => {
+        console.log('Error:', error);
+      }
+    );
+  }
 }
